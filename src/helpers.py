502b37c2e6a3dbb5507cf8f3e1bd99bd15ce5396
JAkkerman <akkermanjoos@gmail.com>
2023-01-25 22:07:20 +0100

    Simulates now both bifurcation and non-bifurcation, using both standard forcing parameters and ranges for forcing parameters

--- src/helpers.py
+++ src/helpers.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_timeseries(simtypes:list):
 
     n_types = len(simtypes)
     fig, ax = plt.subplots(n_types, 1, figsize=(7, 2 * n_types))
 
+    if type(ax) != np.ndarray:
+        ax = [ax]
+
     for i, simtype in enumerate(simtypes):
-        ax[i].plot(simtype.timeseries[:, 1:].T)
+
+        # Set colors for bifurcations and non-bifurcations
+        ax[i].set_prop_cycle(color=np.concatenate((
+                                    ['red' for _ in range(simtype.n_bifurcate)], 
+                                    ['blue' for _ in range(simtype.n_per_model - simtype.n_bifurcate)]))
+                            )
+        
+        # Plot time series
+        ax[i].plot(simtype.timeseries[:, 1:].T, alpha=0.5)
         ax[i].set_xlabel('time')
         ax[i].set_ylabel('$y_t$')
         ax[i].set_title(simtype.name)
--- src/simulate.py
+++ src/simulate.py
@@ -7,29 +7,47 @@ from helpers import plot_timeseries
 
 
 class Simulator:
-    def __init__(self, n_per_model:int, frac_bifurcate:float, tmax:int, seed:int):
+    def __init__(self, n_per_model:int, frac_bifurcate:float, tmax:int, seed:int, 
+                 standard_params:bool):
 
-        assert 0<= frac_bifurcate <= 1., "bifurcation fracton must lie in [0, 1]"
+        assert 0 <= frac_bifurcate <= 1., "bifurcation fracton must lie in [0, 1]"
 
-        self.name = ''
-        self.n_per_model = n_per_model
-        self.frac_bifurcate = frac_bifurcate
-        self.n_bifurcate = int(frac_bifurcate * n_per_model)
-        self.tmax = tmax
-        self.seed = seed
+        self.name = ''                                          # Name of model
+        self.n_per_model = n_per_model                          # Number of simulations per model
+        self.frac_bifurcate = frac_bifurcate                    # Fraction of total sim with bifurcations
+        self.n_bifurcate = int(frac_bifurcate * n_per_model)    # Total number of sim with bifurcations
+        self.tmax = tmax                                        # Max simulation length
+        self.seed = seed                                        # Seed used by numpy random generator
+        self.standard_params = standard_params                  # Use only standard parameters given by model, or stochastic range of forcer
 
         self.timeseries = None
+
+        self.f_min = None
+        self.f_bif_min = None
+        self.f_max = None
+        self.f_patch = None
+        self.f_range_bif = None
+        self.f_range_nobif = None
+
 
     def generate_timeseries(self):
 
         # Set indicators of bifurcation
         self.timeseries[:self.n_bifurcate, 0] = 1
 
-        for i in range(self.n_bifurcate):
-            self.timeseries[i, 1:] = self.gen_bifurcation()
+        for i, j in zip(range(self.n_bifurcate), range(self.n_bifurcate, self.n_per_model)):
 
-        for i in range(self.n_bifurcate, self.n_per_model):
-            self.timeseries[i, 1:] = self.gen_nonbifurcation()
+            # If nonstandard params used, generate new
+            if not self.standard_params:
+                self.set_nonstandard_params()
+
+            # Generate time series
+            self.timeseries[i, 1:] = self.gen_bifurcation()
+            self.timeseries[j, 1:] = self.gen_nonbifurcation()
+
+    def set_nonstandard_params(self):
+        self.f_range_bif = [self.f_min, self.f_bif_min + (self.f_max - self.f_bif_min) * np.random.rand()]
+        self.f_range_nobif = [self.f_min, self.f_min + (self.f_bif_min - self.f_min - self.f_patch) * np.random.rand()]
 
     def gen_bifurcation(self):
         pass
@@ -46,70 +64,96 @@ class Simulator:
 
 class May(Simulator):
     def __init__(self, n_per_model:int, frac_bifurcate:float, 
-                 tmax:int, dt:int, seed:int):
-        super().__init__(n_per_model, frac_bifurcate, tmax, seed)
+                 tmax:int, dt:int, seed:int, standard_params:bool=False):
+        super().__init__(n_per_model, frac_bifurcate, tmax, seed, standard_params)
         
         self.name = 'May'
         self.dt = dt
         self.timeseries = np.zeros((n_per_model, 1 + int(tmax / dt)))
+        self.f_min = 0.15
+        self.f_bif_min = 0.27
+        self.f_max = 0.7
+        self.f_patch = 0.01
+
+        # Set up standard forcing parameter (h) ranges as used by default in ewstools package
+        self.f_range_bif = [self.f_min, self.f_bif_min]
+        self.f_range_nobif = [self.f_min, self.f_min]
 
     def gen_bifurcation(self):
-        return simulate_may(tmax=self.tmax, dt=self.dt).x
+        return simulate_may(tmax=self.tmax, dt=self.dt, h=self.f_range_bif).x
 
     def gen_nonbifurcation(self):
-        return simulate_may(tmax=self.tmax, dt=self.dt).x
+        return simulate_may(tmax=self.tmax, dt=self.dt, h=self.f_range_nobif).x
 
 
 class Ricker(Simulator):
     def __init__(self, n_per_model:int, frac_bifurcate:float, 
-                 tmax:int, seed:int):
-        super().__init__(n_per_model, frac_bifurcate, tmax, seed)
+                 tmax:int, seed:int, standard_params:bool=False):
+        super().__init__(n_per_model, frac_bifurcate, tmax, seed, standard_params)
         
         self.name = 'Ricker'
         self.timeseries = np.zeros((n_per_model, 1 + tmax))
+        self.f_min = 0.
+        self.f_bif_min = 2.7
+        self.f_max = 6.
+        self.f_patch = 0.3
+
+        # Set up standard forcing parameter (F) ranges as used by default in ewstools package
+        self.f_range_bif = [self.f_min, self.f_bif_min]
+        self.f_range_nobif = [self.f_min, self.f_min]
 
     def gen_bifurcation(self):
-        return simulate_ricker(tmax=self.tmax)
+        return simulate_ricker(tmax=self.tmax, F=self.f_range_bif)
 
     def gen_nonbifurcation(self):
-        return simulate_ricker(tmax=self.tmax)
+        return simulate_ricker(tmax=self.tmax, F=self.f_range_nobif)
 
 
 class RosenMac(Simulator):
     def __init__(self, n_per_model:int, frac_bifurcate:float, 
-                 tmax:int, dt:int, seed:int):
-        super().__init__(n_per_model, frac_bifurcate, tmax, seed)
+                 tmax:int, dt:int, seed:int, standard_params:bool=False):
+        super().__init__(n_per_model, frac_bifurcate, tmax, seed, standard_params)
         
         self.name = 'Rosen-Mac'
         self.dt = dt
         self.timeseries = np.zeros((n_per_model, 1 + int(tmax / dt)))
+        self.f_min = 12.
+        self.f_bif_min = 16.
+        self.f_max = 18.
+        self.f_patch = 0.5
+
+        # Set up standard forcing parameter (a) ranges as used by default in ewstools package
+        self.f_range_bif = [self.f_min, self.f_bif_min]
+        self.f_range_nobif = [self.f_min, self.f_min]
 
     def gen_bifurcation(self):
-        return simulate_rosen_mac(tmax=self.tmax, dt=self.dt).x
+        return simulate_rosen_mac(tmax=self.tmax, dt=self.dt, a=self.f_range_bif).x
 
     def gen_nonbifurcation(self):
-        return simulate_rosen_mac(tmax=self.tmax, dt=self.dt).x
+        return simulate_rosen_mac(tmax=self.tmax, dt=self.dt, a=self.f_range_nobif).x
 
     
 if __name__ == "__main__":
 
-    n_per_model = 2
+    # Set parameters used in simulation
+    n_per_model = 10
     frac_bifurcate = 0.5
     tmax = 500
     dt = 0.01
     seed = 1234
+    standard_params = False
 
-    # Generate simulation results
-    # sim = Simulator(n_per_model, frac_bifurcate, tmax, dt)
-    # sim.gen_all_models()
-    # sim.plot_timeseries()
-
+    # Generate simulator objects
     simulators = [
-                    May(n_per_model, frac_bifurcate, tmax, dt, seed),
-                    Ricker(n_per_model, frac_bifurcate, tmax, seed),
-                    RosenMac(n_per_model, frac_bifurcate, tmax, dt, seed)
+                    May(n_per_model, frac_bifurcate, tmax, dt, seed, standard_params=standard_params),
+                    Ricker(n_per_model, frac_bifurcate, tmax, seed, standard_params=standard_params),
+                    RosenMac(n_per_model, frac_bifurcate, tmax, dt, seed, standard_params=standard_params)
                  ]
+    
+    # Generate simulation data and save time series
     for simulator in simulators:
         simulator.generate_timeseries()
         simulator.save_simdata()
+
+    # Plot generated time series
     plot_timeseries(simulators)
